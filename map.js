b1918335c72d17eb649055deaf79d7f8c5ed856c
guuuy663 <ella.dreijer@icloud.com>
2026-01-29 20:04:39 +0200

    Update map.js

--- map.js
+++ map.js
@@ -1,40 +1,47 @@
+const fs = require("fs");
+const FILE = "savedMap.json";
+
 function generateMap() {
-  const objects = [];
+  const objs = [];
 
-  // Floor
-  objects.push({
-    type: "floor",
-    x: 0, y: 0, z: 0,
-    w: 100, d: 100
-  });
+  objs.push({ type:"floor", x:0,z:0,w:100,d:100 });
 
-  // Walls / cover
-  for (let i = 0; i < 25; i++) {
-    objects.push({
-      type: "box",
-      x: Math.random() * 80 - 40,
-      y: 1,
-      z: Math.random() * 80 - 40,
-      w: 2 + Math.random() * 4,
-      h: 2 + Math.random() * 3,
-      d: 2 + Math.random() * 4
+  for(let i=0;i<20;i++){
+    objs.push({
+      type:"box",
+      x:Math.random()*80-40,
+      y:1,
+      z:Math.random()*80-40,
+      w:3,h:3,d:3
     });
   }
 
-  // Platforms
-  for (let i = 0; i < 10; i++) {
-    objects.push({
-      type: "platform",
-      x: Math.random() * 60 - 30,
-      y: 3 + Math.random() * 4,
-      z: Math.random() * 60 - 30,
-      w: 6,
-      h: 0.5,
-      d: 6
+  for(let i=0;i<8;i++){
+    objs.push({
+      type:"loot",
+      x:Math.random()*60-30,
+      y:1,
+      z:Math.random()*60-30
     });
   }
 
-  return objects;
+  return objs;
+}
+
+function loadMap(){
+  if(fs.existsSync(FILE))
+    return JSON.parse(fs.readFileSync(FILE));
+  const map = generateMap();
+  saveMap(map);
+  return map;
+}
+
+function saveMap(map){
+  fs.writeFileSync(FILE, JSON.stringify(map));
+}
+
+module.exports = { loadMap, saveMap };
+
 }
 
 module.exports = generateMap;
